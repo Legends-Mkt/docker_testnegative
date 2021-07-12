5b1ecaf80761cceec0006dbcae46af63a62f17e5
Claude Lefort <84156115+roblefort@users.noreply.github.com>
2021-07-12 16:57:19 -0500

    Delete mnt directory

--- mnt/composer.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-cd /opt && \
-curl -sS https://getcomposer.org/installer -o composer-setup.php && \
-HASH="$(wget -q -O - https://composer.github.io/installer.sig)" && \
-php -r "if (hash_file('SHA384', 'composer-setup.php') === '$HASH') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;" && \
-php composer-setup.php --install-dir=/usr/local/bin --filename=composer && \
-ln -s /usr/local/bin/composer /usr/bin/
-apt update && apt install -y git
